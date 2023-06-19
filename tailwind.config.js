edb2e3d5d774c21e4caf0b59a41ffbf5ea586026
Stanley Masinde <stanmasinde@gmail.com>
2023-06-20 01:19:23 +0300

    fix: the production bundle lacks styling for links  - I have added the src js files to the scan files for Tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: [
+    './index.html',
+    './src/**/*.{html,js}'
+  ],
   theme: {
     extend: {},
   },
