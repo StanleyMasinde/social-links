963e03be84f0582ec4325d8209aee9c089c6ce82
Stanley Masinde <stanmasinde@gmail.com>
2023-06-20 01:58:17 +0300

    chore: Code quality  - add comments to main.js

--- src/main.js
+++ src/main.js
@@ -1,35 +1,39 @@
 window.addEventListener('load', function () {
     (async function main() {
-        document.querySelector('#year')
-            .textContent = new Date().getFullYear()
-        await fetchLinks()
-    }())
+        // Set the current year in the #year element
+        document.querySelector('#year').textContent = new Date().getFullYear();
+        await fetchLinks();
+    })();
 
-    const createLinkElements = (links = []) => {
-        const linksDiv = this.document.querySelector('#links')
+    // Function to create link elements and append them to the #links div
+    function createLinkElements(links = []) {
+        const linksDiv = document.querySelector('#links');
         links.forEach(link => {
-            const linkElement = this.document.createElement('a')
-            linkElement.classList.add('button_link')
-            linkElement.href = link.url
-            linkElement.text = link.name
-            linkElement.target = "_blank"
-            linkElement.rel = "noopener noreferrer"
-            linksDiv.appendChild(linkElement)
+            const linkElement = document.createElement('a');
+            linkElement.classList.add('button_link');
+            linkElement.href = link.url;
+            linkElement.textContent = link.name;
+            linkElement.target = '_blank';
+            linkElement.rel = 'noopener noreferrer';
+            linksDiv.appendChild(linkElement);
         });
     }
 
+    // Async function to fetch links data from links.json
     async function fetchLinks() {
         try {
-            const response = await fetch('/src/links.json')
+            const response = await fetch('/src/links.json');
 
             if (response.ok) {
-                const linksResponse = await response.json()
-                createLinkElements(linksResponse.links)
+                const linksResponse = await response.json();
+                createLinkElements(linksResponse.links);
             } else {
-                // A non 200 response was returned
+                // Handle the case when a non-200 response was returned
+                console.error('Error: Unable to fetch links data.');
             }
         } catch (error) {
-            // This is probably a syntax error
+            // Handle any errors occurred during the fetch or JSON parsing
+            console.error('Error:', error);
         }
     }
-})
+});
